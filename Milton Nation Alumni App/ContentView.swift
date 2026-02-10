219fab1e1888ef2edc9ae139ba990e233f5fdb76
ezra <media@miltonhealthgroup.com>
2026-02-10 15:48:17 -0500

    Initial Commit

---
+++ Milton Nation Alumni App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milton Nation Alumni App
+//
+//  Created by Ezra Barishansky  on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Milton Nation Alumni App/Milton_Nation_Alumni_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Milton_Nation_Alumni_AppApp.swift
+//  Milton Nation Alumni App
+//
+//  Created by Ezra Barishansky  on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Milton_Nation_Alumni_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
